cf546042f4ee60a710fa08395cabd49ca758703f
Phong Penglerd <Phong Penglerd>
2017-05-30 13:46:36 +0200

    Initial commit

---
+++ app/src/main/java/lor/ch/leagueofranks/SummonerNormal.java
@@ -0,0 +1,13 @@
+package lor.ch.leagueofranks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SummonerNormal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summoner_normal);
+    }
+}
